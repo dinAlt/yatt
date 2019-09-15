36e293b977ba7a41ae43d612aaba42d8fbcbc0e0
dinAlt <dinalt2@gmail.com>
2019-09-15 06:06:27 +0300

    add clippy to pre commit hooks

--- src/commands/list/intervals.rs
+++ src/commands/list/intervals.rs
@@ -8,6 +8,32 @@ pub fn register<'a>(app: App<'a, 'a>) -> App {
     app.subcommand(
         SubCommand::with_name("intervals")
             .setting(AppSettings::AllowNegativeNumbers)
-            .about("Show intervals list"),
+            .about("Show intervals list")
+            .arg(
+                Arg::with_name("period")
+                    .short("p")
+                    .long("period")
+                    .help("Show only intervals that started in a given period")
+                    .takes_value(true)
+                    .multiple(true),
+            )
+            .arg(
+                Arg::with_name("closed")
+                    .short("c")
+                    .long("closed")
+                    .help("Show intervals for closed tasks"),
+            )
+            .arg(
+                Arg::with_name("deleted")
+                    .short("d")
+                    .long("deleted")
+                    .help("Show deleted intervals"),
+            )
+            .arg(
+                Arg::with_name("active")
+                    .short("a")
+                    .long("active")
+                    .help("Show active intervals (default)"),
+            ),
     )
 }
