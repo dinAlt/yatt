a5cbc9ee74250dbca682a5088cc38c9fc7466d68
dinAlt <dinalt2@gmail.com>
2020-05-28 04:15:13 +0300

    Add: optional ID parameter for restart command

--- src/commands/restart.rs
+++ src/commands/restart.rs
@@ -4,7 +4,7 @@ use yatt_orm::statement::*;
 
 pub(crate) fn exec<T: DBRoot, P: Printer>(
   ctx: &AppContext<T, P>,
-  _args: &ArgMatches,
+  args: &ArgMatches,
 ) -> CliResult<()> {
   let res = ctx
     .db
@@ -22,35 +22,46 @@ pub(crate) fn exec<T: DBRoot, P: Printer>(
     });
   };
 
-  let interval: Vec<Interval> = ctx.db.get_by_statement(
-    filter(and(
-      ne(Interval::deleted_n(), 1),
-      ne(Interval::closed_n(), 1),
-    ))
-    .sort(&Interval::end_n(), SortDir::Descend)
-    .limit(1),
-  )?;
+  let node_id = if args.is_present("ID") {
+    args
+      .value_of("ID")
+      .unwrap()
+      .parse()
+      .map_err(|_| CliError::Parse {
+        message: "Unable to parse task ID".into(),
+      })?
+  } else {
+    let interval: Vec<Interval> = ctx.db.get_by_statement(
+      filter(and(
+        ne(Interval::deleted_n(), 1),
+        ne(Interval::closed_n(), 1),
+      ))
+      .sort(&Interval::end_n(), SortDir::Descend)
+      .limit(1),
+    )?;
 
-  if interval.is_empty() {
-    return Err(CliError::Task {
-      source: TaskError::Cmd {
-        message: "There is no priviosly started tasks.".to_string(),
-      },
-    });
-  }
+    if interval.is_empty() {
+      return Err(CliError::Task {
+        source: TaskError::Cmd {
+          message: "There is no priviosly started tasks.".to_string(),
+        },
+      });
+    }
+    interval.first().unwrap().to_owned().node_id.unwrap()
+  };
 
-  let mut interval = interval.first().unwrap().to_owned();
-  let now = Utc::now();
-  interval.id = 0;
-  interval.begin = now;
-  interval.end = None;
+  let mut interval = Interval::default();
+  interval.node_id = Some(node_id);
 
   let node: Vec<Node> = ctx
     .db
     .get_by_filter(eq(Node::id_n(), interval.node_id.unwrap()))?;
   if node.is_empty() {
     return Err(CliError::Unexpected {
-      message: format!("node with id {}", interval.node_id.unwrap()),
+      message: format!(
+        "node with id {} not found",
+        interval.node_id.unwrap()
+      ),
     });
   };
 
@@ -73,6 +84,11 @@ pub(crate) fn exec<T: DBRoot, P: Printer>(
 
 pub fn register<'a>(app: App<'a, 'a>) -> App {
   app.subcommand(
-    SubCommand::with_name("restart").about("Restarts last task"),
+    SubCommand::with_name("restart")
+      .about("Restarts last task")
+      .arg(
+        Arg::with_name("ID")
+          .help("Task id. By default, the last runned task is used"),
+      ),
   )
 }
